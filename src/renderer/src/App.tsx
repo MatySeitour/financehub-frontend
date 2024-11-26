7ed7b4b86a727bd006548f23cc597680f5024c1d
Matias Seitour <matias.seitour01@gmail.com>
2024-11-26 09:13:02 -0300

    feat(layout) create basic protected layout
    -create navmenu

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,10 +1,21 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import { ProtectedLayout } from "./components/layouts/ProtectedLayout";
 
-function App(): JSX.Element {
+function App() {
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<>/</>} />
+        <Route path="/" element={<Navigate to={"/inicio"} />} />
+        <Route element={<ProtectedLayout />}>
+          <Route path="/inicio" element={<main className="w-full h-full bg-green-200">Estamos en home</main>} />
+          <Route path="/clientes" element={<main className="w-full h-full bg-green-200">Estamos en clientes</main>} />
+          <Route path="/vendedores" element={<main className="w-full h-full bg-green-200">Estamos en vendedores</main>} />
+          <Route path="/prestamos" element={<main className="w-full h-full bg-green-200">Estamos en prestamos</main>} />
+          <Route path="/cajas" element={<main className="w-full h-full bg-green-200">Estamos en cajas</main>} />
+          <Route path="/operaciones" element={<main className="w-full h-full bg-green-200">Estamos en operaciones</main>} />
+          <Route path="/configuracion" element={<main className="w-full h-full bg-green-200">Estamos en configuracion</main>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
