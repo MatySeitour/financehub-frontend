cfac94326178adcf49011933fe13f7496f9e627a
facundo <facu.iglesias2001@gmail.com>
2025-04-25 08:42:27 -0300

    fix (protected layout) repair protected layout

--- src/renderer/src/components/layouts/ProtectedLayout.tsx
+++ src/renderer/src/components/layouts/ProtectedLayout.tsx
@@ -40,7 +40,11 @@ export default function ProtectedLayout() {
       console.log("Error en sesión:", error);
       if (errorAuth.includes(error?.message ?? "")) {
         navigate("/create-organization");
-      } else if (error?.status === 401 || error?.status === 403) {
+      } else if (
+        error?.status === 401 ||
+        error?.status === 403 ||
+        error?.status === 404
+      ) {
         navigate("/login");
       }
     },
@@ -77,83 +81,75 @@ export default function ProtectedLayout() {
   }
 
   return (
-    <div className="flex h-screen w-screen">
-      <nav className="relative flex h-full min-w-72 flex-col gap-4 border-r">
-        <div className="flex w-full flex-col gap-4 border-b p-4">
-          <div className="flex items-center justify-between">
-            <div className="flex items-center gap-3">
-              <div className="h-6 w-6 rounded-full bg-red-200"></div>
-              <div className="text-center">
-                <p className="text-sm font-semibold text-slate-800">
-                  {user?.organization?.name}
-                </p>
-              </div>
-            </div>
-            <IoLogOutOutline
-              onClick={() => onOpen()}
-              className="size-6 min-w-6 cursor-pointer text-slate-300 transition-colors hover:text-red-500"
-            />
-          </div>
-          {/* <div className="h-[1px] w-full bg-slate-200"></div> */}
-        </div>
-        <div className="flex h-full w-full flex-col justify-between p-4">
-          <ul className="flex flex-col gap-2">
-            {navItems.map((navItem) => (
-              <li key={navItem.name} className="h-auto w-full">
-                <NavLink
-                  className={({ isActive }) =>
-                    `${isActive ? `rounded-md bg-slate-100 text-slate-700` : `text-slate-500`} flex h-auto w-full items-center px-2 py-1 font-medium transition-all hover:bg-slate-100/60 hover:text-slate-700`
-                  }
-                  to={navItem.linkTo}
-                >
-                  <navItem.icon className="size-5 min-w-5" />
-                  <p className="rounded-lg px-4 py-2 text-sm">{navItem.name}</p>
-                </NavLink>
-              </li>
-            ))}
-          </ul>
-          <div className="flex h-12 w-full items-center gap-4 rounded-md bg-white px-2 py-1 shadow-md">
-            <div className="rounded-full">
-              <FaUserCircle className="size-6 min-w-6 text-slate-500" />
-            </div>
-            <div className="flex w-full flex-col gap-1">
-              <div className="group relative h-4 w-full overflow-hidden">
-                <p className="absolute top-0 w-full cursor-default text-sm font-semibold text-slate-600 transition-transform group-hover:translate-y-full">
-                  {user?.name}
-                </p>
-                <p className="absolute top-0 w-full -translate-y-full cursor-default text-sm font-semibold text-slate-600 transition-transform group-hover:-translate-y-[0.100rem]">
-                  {user?.email}
-                </p>
-              </div>
-              <p className="text-[0.65rem] font-medium text-slate-400">
-                Administrador
-              </p>
-            </div>
+    <div className="flex h-screen w-screen bg-white">
+      {sessionQuery?.isError ? (
+        <div className="flex h-full w-full items-center justify-center">
+          <div className="flex h-96 w-full max-w-2xl flex-col items-center justify-center gap-2 rounded-md bg-gradient-to-t from-red-200/50 via-white to-white">
+            <MdError className="size-24 min-w-24 text-red-500" />
+            <p className="text-2xl font-medium text-red-500">
+              Ha ocurrido un error en el servidor
+            </p>
           </div>
         </div>
-      </nav>
-      <div className="flex w-2/3 flex-grow flex-col">
-        <div className="flex flex-grow flex-col overflow-hidden">
-          <Outlet />
-        </div>
-      </div>
-      <Modal
-        backdrop="opaque"
-        radius="sm"
-        isOpen={isOpen}
-        onOpenChange={onOpenChange}
-      >
-        <ModalContent>
-          {(onClose) => (
-            <div className="flex flex-col gap-2 px-2 py-4">
-              <div className="flex justify-center">
-                <div className="flex items-center justify-center rounded-full bg-red-300/30 p-3">
-                  <IoLogOutOutline className="size-12 min-w-12 pl-1 text-red-500" />
+      ) : (
+        <>
+          <nav className="relative flex h-full min-w-72 flex-col gap-4 border-r">
+            <div className="flex w-full flex-col">
+              <div className="flex items-center justify-between p-5">
+                <div className="flex items-center gap-3">
+                  <div className="h-6 w-6 rounded-full bg-red-200"></div>
+                  <div className="text-center">
+                    <p className="text-sm font-semibold text-slate-800">
+                      {sessionQuery?.data?.user?.organization?.name}
+                    </p>
+                  </div>
+                </div>
+                <IoLogOutOutline
+                  onClick={() => onOpen()}
+                  className="size-6 min-w-6 cursor-pointer text-slate-300 transition-colors hover:text-red-500"
+                />
+              </div>
+              <div className="h-[1px] w-full bg-slate-200"></div>
+            </div>
+            <div className="flex h-full w-full flex-col justify-between p-4">
+              <ul className="flex flex-col gap-2">
+                {navItems.map((navItem) => (
+                  <li key={navItem.name} className="h-auto w-full">
+                    <NavLink
+                      className={({ isActive }) =>
+                        `${isActive ? `rounded-md bg-slate-100 text-slate-700` : `text-slate-500`} flex h-auto w-full items-center px-2 py-1 font-medium transition-all hover:bg-slate-100/60 hover:text-slate-700`
+                      }
+                      to={navItem.linkTo}
+                    >
+                      <navItem.icon className="size-5 min-w-5" />
+                      <p className="rounded-lg px-4 py-2 text-sm">
+                        {navItem.name}
+                      </p>
+                    </NavLink>
+                  </li>
+                ))}
+              </ul>
+              <div className="flex h-12 w-full items-center gap-4 rounded-md bg-white px-2 py-1 shadow-md">
+                <div className="rounded-full">
+                  <FaUserCircle className="size-6 min-w-6 text-slate-500" />
+                </div>
+                <div className="flex w-full flex-col gap-1">
+                  <div className="group relative h-4 w-full overflow-hidden">
+                    <p className="absolute top-0 w-full cursor-default text-sm font-semibold text-slate-600 transition-transform group-hover:translate-y-full">
+                      {sessionQuery?.data?.user?.name}
+                    </p>
+                    <p className="absolute top-0 w-full -translate-y-full cursor-default text-sm font-semibold text-slate-600 transition-transform group-hover:-translate-y-[0.100rem]">
+                      {sessionQuery?.data?.user?.email}
+                    </p>
+                  </div>
+                  <p className="text-[0.65rem] font-medium text-slate-400">
+                    Administrador
+                  </p>
                 </div>
               </div>
             </div>
           </nav>
-          <div className="flex w-2/3 flex-grow flex-col px-6 py-2">
+          <div className="flex w-2/3 flex-grow flex-col py-2">
             <div className="flex flex-grow flex-col overflow-hidden rounded-md bg-white">
               <Outlet />
             </div>
